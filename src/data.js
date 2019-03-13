4382221c942415aade36781b7fe901557e493f88
FabyTapia <ftapiamorales@gmail.com>
2019-03-12 22:32:40 -0300

    se limpia codigo se cambian clases al ingles

--- src/data.js
+++ src/data.js
@@ -1,19 +1,5 @@
 
-/* Manejo de data
 
- esta es una función de ejemplo
- puedes ver como agregamos la función a nuestro objeto global window*/
-
-
-/*function filter(data2, condition){
-const resultTags = data2.filter((element)=>{
-    return element.tags.includes(condition)===true;
-
-});
-
-return resultTags;
-}
-*/
 const filterData = (data2,condition) => {
   let resultTags = data2.filter(element =>{
     return (element).tags.includes(condition)=== true;
@@ -35,5 +21,7 @@ return orderChampions
 
 
 window.sortData=sortData;
+
+
 
 
--- src/main.js
+++ src/main.js
@@ -1,71 +1,35 @@
-  /*window.onload = () => {*/
-const data2= Object.values(window.LOL.data);
+
+const dataLol= Object.values(window.LOL.data);
 
 const boardCharacters = document.getElementById("board");
 document.getElementById("power-type").addEventListener("change", () => {
-  
+
   let condition = document.getElementById("power-type").value;
-  let resultTags=window.filterData(data2,condition);
+  let resultTags=window.filterData(dataLol,condition);
   boardCharacters.innerHTML ="";
   resultTags.forEach(element => {
-<<<<<<< Updated upstream
-    boardCharacters.innerHTML+= `<div class="col-sm-12	col-md-4 col-lg-4	col-xl-4">
-    <div class="card text-white bg-transparent">
-      <div class="card-body ">
-        <h5 class="card-title font-weight-bold">${element.name}</h5>
+    boardCharacters.innerHTML+= `<div class="col-sm-12	col-md-4	col-lg-4	col-xl-4">
+    <div class="card text-white bg-transparent"
+      <div class="card-body">
+        <h3 class="card-title font-weight-bold ">${element.name}</h3>
+
         <img class="image" src="${element.splash}" alt=""class="img-thumbnail">
         <h5 class="card-title font-weight-bold">${element.blurb}</h5>
       </div>
-    </div>`
-  
-=======
-    boardCharacters.innerHTML+= `<div class="card-deck">
-    <div class="card">
-      <img class="card-img-top" src="${element.splash}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${element.name}</h5>
-        <p class="card-text">${element.blurb}</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">
-        LOL Champions</small>
-      </div>
-    </div>
-    <div class="card">
-    <img class="card-img-top" src="${element.splash}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${element.name}</h5>
-      <p class="card-text">${element.blurb}</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">LOL Champions</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="${element.splash}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${element.name}</h5>
-      <p class="card-text">${element.blurb}</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">LOL Champions</small>
-    </div>
-  </div>
-</div>`
+      </div>`
 
->>>>>>> Stashed changes
   });
 });
 
 const championsOrder = document.getElementById("orden1");
-const containerOrder= document.getElementById("championBox") 
+const containerOrder= document.getElementById("championBox")
 
 
 championsOrder.addEventListener('change', () => {
   let option = championsOrder.value;
-  let ordering = window.sortData(data2, 'name', option); 
+  let ordering = window.sortData(dataLol, 'name', option);
   containerOrder.innerHTML = '';
-   ordering.forEach(element => { 
+   ordering.forEach(element => {
     containerOrder.innerHTML += `<div class="col-sm-12	col-md-4	col-lg-4	col-xl-4">
     <div class="card text-white bg-transparent"
       <div class="card-body">
@@ -74,10 +38,10 @@ championsOrder.addEventListener('change', () => {
         <img class="image" src="${element.splash}" alt=""class="img-thumbnail">
         <h5 class="card-title font-weight-bold">${element.blurb}</h5>
       </div>
-      </div>
-    </div>`
-    
-       
-       
+      </div>`
+
+
+
+
    });
   });
